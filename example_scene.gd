5016c184aea5c14556927b41baeabf1be707abfc
Ryan Remer <ryandremer@gmail.com>
2023-09-21 22:05:39 -0700

    Made the terrain noisey
    took a while to disable the smooth group
    
    time: 1 hour

--- example_scene.gd
+++ example_scene.gd
@@ -21,21 +21,3 @@ func _ready():
 		point_view.add_point(main_position + axis, Colors.LIGHT_PINK);
 		
 	point_view.add_point(main_position + total_offset, Colors.GOLD_A);
-		
-	
-func get_surface_position(index: Vector3i):
-	const AXIS = [Vector3i(1,0,0),Vector3i(0,1,0),Vector3i(0,0,1)]
-	var main_sample_value = get_sample_value(index);
-	var total_offset = Vector3(0,0,0);
-	
-	for axis_index in range(3):
-		var axis = AXIS[axis_index];
-		var neighbor_sample_value = get_sample_value(index + axis);
-		var ratio = (0.0 - main_sample_value) / (neighbor_sample_value - main_sample_value);
-		var offset: Vector3 = axis * ratio;
-		total_offset += offset;
-	
-	return index + total_offset;
-		
-func get_sample_value(index: Vector3i):
-	pass;
--- surface_net.gd
+++ surface_net.gd
@@ -1,22 +1,116 @@
 extends MeshInstance3D
 
 var surfaceTool := SurfaceTool.new();
-@export var point_view: PointView;
+@onready var point_view: PointView = $"../PointView";
+@export var noise: Noise;
 
 func _ready():
 	surfaceTool.begin(Mesh.PRIMITIVE_TRIANGLES);
 	surfaceTool.set_color(Colors.BLUE_D);
+	surfaceTool.set_smooth_group(-1)
 	create_surface_mesh();
+	surfaceTool.generate_normals();
 	mesh = surfaceTool.commit();
 
 # Step 1: Define Surface Distance
 const CENTER := Vector3.ZERO;
 const RADIUS: float = 5.0;	
 
-func get_sample_value(index: Vector3i) -> float:
-	return RADIUS - CENTER.distance_to(index);
-
+func get_sample_value(index: Vector3i) -> float:	
+	return noise.get_noise_3dv(index);
+	return CENTER.distance_to(index) - RADIUS;
+	
 # Step 6: Create Surface
+func create_surface_mesh(size: int = 16):
+	for x in range(-size, size):
+		for y in range(-size, size):
+			for z in range(-size, size):
+				create_surface_mesh_quad(Vector3i(x,y,z));
+				
+				
+func create_surface_mesh_quad(index: Vector3i):
+	for axis_index in range(AXIS.size()):
+		var axis = AXIS[axis_index];
+		var sample_value1 = get_sample_value(index);
+		var sample_value2 = get_sample_value(index + axis);
+		
+		if sample_value1 < 0 and sample_value2 >= 0:
+			add_quad(index, axis_index);
+		elif sample_value1 >= 0 and sample_value2 < 0:
+			add_reversed_quad(index, axis_index);
+			
+func add_quad(index: Vector3i, axis_index: int):
+	var points = get_quad_points(index, axis_index);
+	
+	surfaceTool.set_normal(AXIS[axis_index]);
+	
+	add_vertex(points[0])
+	add_vertex(points[1])
+	add_vertex(points[2])
+
+	add_vertex(points[0])
+	add_vertex(points[2])
+	add_vertex(points[3])
+	
+func add_reversed_quad(index: Vector3i, axis_index: int):
+	var points = get_quad_points(index, axis_index);
+	
+	surfaceTool.set_normal(-AXIS[axis_index]);
+	
+	add_vertex(points[0])
+	add_vertex(points[2])
+	add_vertex(points[1])
+
+	add_vertex(points[0])
+	add_vertex(points[3])
+	add_vertex(points[2])
+			
+func get_quad_points(index: Vector3i, axis_index: int):
+	return [
+		index + QUAD_POINTS[axis_index][0],
+		index + QUAD_POINTS[axis_index][1],
+		index + QUAD_POINTS[axis_index][2],
+		index + QUAD_POINTS[axis_index][3],
+	];
+
+# Step 7 Shift the surface points and generate normals			
+func add_vertex(index: Vector3i):	
+	var sample_value = get_sample_value(index);
+	var sample_position = Vector3(index);
+	
+	var surface_position = get_surface_position(index);
+	
+	var surface_gradient = get_surface_gradient(index, sample_value);
+	surfaceTool.add_vertex(surface_position);
+	
+func get_surface_position(index: Vector3i):
+	var total := Vector3.ZERO;
+	var surface_edge_count = 0;
+	
+	for edge_offsets in EDGES:
+		var position_a = Vector3(index + edge_offsets[0]);
+		var sample_a = get_sample_value(position_a);
+		var position_b = Vector3(index + edge_offsets[1])
+		var sample_b = get_sample_value(position_b);
+		
+		if sample_a * sample_b <= 0:
+			# if different signs
+			surface_edge_count += 1;
+			total += position_a.lerp(position_b, abs(sample_a) / (abs(sample_a) + abs(sample_b)));
+	
+	if surface_edge_count == 0:
+		return Vector3(index) + Vector3.ONE * 0.5;
+	
+	return total / surface_edge_count;
+	
+func get_surface_gradient(index: Vector3i, sample_value: float) -> Vector3:
+	return Vector3(
+		sample_value - get_sample_value(index + AXIS[0]),
+		sample_value - get_sample_value(index + AXIS[1]),
+		sample_value - get_sample_value(index + AXIS[2])
+	).normalized();	
+
+# Constants
 const AXIS := [
 	Vector3i(1,0,0),	
 	Vector3i(0,1,0),	
@@ -48,66 +142,20 @@ const QUAD_POINTS := [
 	],	
 ];
 
-func create_surface_mesh_quad(index: Vector3i):
-	for axis_index in range(AXIS.size()):
-		var axis = AXIS[axis_index];
-		var sample_value1 = get_sample_value(index);
-		var sample_value2 = get_sample_value(index + axis);
-		
-		var points = [
-				index + QUAD_POINTS[axis_index][0],
-				index + QUAD_POINTS[axis_index][1],
-				index + QUAD_POINTS[axis_index][2],
-				index + QUAD_POINTS[axis_index][3],
-			];
-		
-		if sample_value1 < 0 and sample_value2 >= 0:
-			surfaceTool.set_normal(axis * -1);
-			add_quad(points);
-		elif sample_value1 >= 0 and sample_value2 <= 0:
-			surfaceTool.set_normal(axis);
-			add_reversed_quad(points);
-
-func add_quad(points):
-	add_vertex(points[0])
-	add_vertex(points[2])
-	add_vertex(points[1])
-
-	add_vertex(points[0])
-	add_vertex(points[3])
-	add_vertex(points[2])
-	
-func add_reversed_quad(points):
-	add_vertex(points[0])
-	add_vertex(points[1])
-	add_vertex(points[2])
-
-	add_vertex(points[0])
-	add_vertex(points[2])
-	add_vertex(points[3])
-	
-func add_vertex(index: Vector3i):
-	var sample_value = get_sample_value(index);
-	var surface_gradient = get_surface_gradient(index, sample_value);
-	var sample_position = Vector3(index);
-	
-	var surface_position = sample_position + surface_gradient * sample_value
-	
-	surfaceTool.set_normal(surface_gradient);
-	surfaceTool.add_vertex(surface_position);
-	point_view.add_point(Vector3(surface_position), Colors.PURPLE_A)
-	
-func get_surface_gradient(index: Vector3i, sample_value: float) -> Vector3:
-	return Vector3(
-		sample_value - get_sample_value(index + AXIS[0]),
-		sample_value - get_sample_value(index + AXIS[1]),
-		sample_value - get_sample_value(index + AXIS[2])
-	).normalized();
-	
-func create_surface_mesh(size: int = 6):
-	for x in range(-size, size):
-		for y in range(-size, size):
-			for z in range(-size, size):
-				create_surface_mesh_quad(Vector3i(x,y,z));
-				
-
+const EDGES := [
+	# Edges on min Z axis
+	[Vector3i(0,0,0),Vector3i(1,0,0)],
+	[Vector3i(1,0,0),Vector3i(1,1,0)],
+	[Vector3i(1,1,0),Vector3i(0,1,0)],
+	[Vector3i(0,1,0),Vector3i(0,0,0)],
+	# Edges on max Z axis
+	[Vector3i(0,0,1),Vector3i(1,0,1)],
+	[Vector3i(1,0,1),Vector3i(1,1,1)],
+	[Vector3i(1,1,1),Vector3i(0,1,1)],
+	[Vector3i(0,1,1),Vector3i(0,0,1)],
+	# Edges connecting min Z to max Z
+	[Vector3i(0,0,0),Vector3i(0,0,1)],
+	[Vector3i(1,0,0),Vector3i(1,0,1)],
+	[Vector3i(1,1,0),Vector3i(1,1,1)],
+	[Vector3i(0,1,0),Vector3i(0,1,1)],
+]
